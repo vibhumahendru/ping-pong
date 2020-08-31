706b3c69a5f73fdf9c0e36ff26307167c4ad0885
vibhumahendru <vibhumahendru@gmail.com>
2020-08-31 12:13:52 +0530

    test

--- ping_pong/settings.py
+++ ping_pong/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pp_score',
-    'rest_framework'
+    'rest_framework',
+    'pp_score'
 ]
 
 MIDDLEWARE = [
