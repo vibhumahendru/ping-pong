5346b092c362dac50f7c3777084084292d67fc2b
vibhumahendru <vibhumahendru@gmail.com>
2020-10-26 15:39:03 +0530

    new fn added

--- pp_score/views.py
+++ pp_score/views.py
@@ -4,14 +4,28 @@ from .models import Session, Game
 from rest_framework import status
 from rest_framework.response import Response
 from django.db.models import F, Sum
+from django.forms.models import model_to_dict
 
 
 class SessionViewset(viewsets.ModelViewSet):
     serializer_class = SessionSerializer
     http_method_names = ['get','post']
 
-    def get_queryset(self):
+    def get_queryset(self, request, *args, **kwargs):
         return Session.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        key = self.request.GET.get('apiKey','')
+
+        if key == 'vibz1!':
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response({"not":"allowed"}, status=status.HTTP_401_UNAUTHORIZED)
+
 
 class GameViewset(viewsets.ModelViewSet):
     serializer_class = GameSerializer
@@ -86,3 +100,19 @@ class ScoreViewset(viewsets.ViewSet):
         result['dad_points_graph'] = dad_points_graph
 
         return Response(result, status=status.HTTP_200_OK)
+
+class ScoreInputViewSet(viewsets.ViewSet):
+
+    def get_current_session(self, request, *args, **kwargs):
+        current_sesion = Session.objects.last()
+        vibhu_games_won_by_session = Game.objects.filter(session=current_sesion, vibhu_score__gte=F('dad_score')).count()
+        dad_games_won_by_session = Game.objects.filter(session=current_sesion, dad_score__gte=F('vibhu_score')).count()
+
+        data = {}
+
+        data['current_session_date'] = current_sesion.date
+        data['current_session_id'] = current_sesion.id
+        data['vibhu_score'] = vibhu_games_won_by_session
+        data['dad_score'] = dad_games_won_by_session
+
+        return Response(data, status=status.HTTP_200_OK)
